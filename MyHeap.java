c03a2cab8e9842addbb6dd98579166852117014f
DarthChurros <tnarayan00@stuy.edu>
2019-04-29 06:44:18 -0400

    I think my pushDown works

--- MyHeap.java
+++ MyHeap.java
@@ -8,15 +8,27 @@ public class MyHeap {
   private static void pushDown(int[] data, int size, int index) {
     boolean run = true;
     while (run && index < size) {
-      if (data[index] >= data[2*index+1] && data[index] < data[2*index+2]) {
-        swap(data, index, 2*index+2);
-        index = 2*index+2;
-        run = false;
-      } else if (data[index] > data[2*index+1] && data[index] >= data[2*index+1]){
+      try {
+        if (data[index] >= data[2*index+1] && data[index] >= data[2*index+2]) {
+          run = false;
+        } else if (data[2*index+1] > data[2*index+2]){
+          swap(data, index, 2*index+1);
+          index = 2*index+1;
+        } else {
+          swap(data, index, 2*index+2);
+          index = 2*index+2;
+        }
+      } catch (IndexOutOfBoundsException e) {
         swap(data, index, 2*index+1);
-        index = 2*index+1;
         run = false;
       }
     }
+  }
+
+  public static void main(String[] args) {
+    int[] test = new int[]{1, 2, 3, 4, 5, 6};
+    HeapPrinter.print(test);
+    pushDown(test, 6, 0);
+    HeapPrinter.print(test);
   }
 }
