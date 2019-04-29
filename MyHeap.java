0af2e7ed425095aab52268ee5036329de17403b2
DarthChurros <tnarayan00@stuy.edu>
2019-04-29 07:20:14 -0400

    fixed pushDown exception handling

--- MyHeap.java
+++ MyHeap.java
@@ -7,6 +7,7 @@ public class MyHeap {
 
   private static void pushDown(int[] data, int size, int index) {
     boolean run = true;
+    //System.out.println("pushing down index "+index);
     while (run && index < size) {
       try {
         if (data[index] >= data[2*index+1] && data[index] >= data[2*index+2]) {
@@ -20,17 +21,19 @@ public class MyHeap {
         }
       } catch (IndexOutOfBoundsException e) {
         try {
-          swap(data, index, 2*index+1);
+          if (data[index] < data[2*index+1]) {
+            swap(data, index, 2*index+1);
+          }
         } catch (IndexOutOfBoundsException f) {}
         run = false;
       }
+      //HeapPrinter.print(data);
     }
   }
 
   public static void heapify(int[] data) {
     for (int i = (int)(Math.log(data.length)/Math.log(2)); i >= 0; i--) {
       pushDown(data, data.length, i);
-      HeapPrinter.print(data);
     }
   }
 
